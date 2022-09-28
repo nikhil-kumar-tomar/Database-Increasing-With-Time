51c6e092abbab9cef0808877a0c584d9a2dfb229
nikhil631 <nikhiltomar931@gmail.com>
2022-09-28 10:24:09 +0530

    changed code from High comp to low comp

--- Data extration using pandas/Pandas Data extraction.py
+++ Data extration using pandas/Pandas Data extraction.py
@@ -1,4 +1,5 @@
+from ast import Index
 import pandas as pd
 df = pd.read_csv('data.csv')
-ld=df.loc[(df["Competition"]=="High") & (df["Avg. monthly searches"]>=4000),["Keyword","Competition","Avg. monthly searches"]]
-ld.to_csv("final.csv")
+ld=df.loc[(df["Competition"]=="Low") & (df["Avg. monthly searches"]>=4000),["Keyword","Competition","Avg. monthly searches"]]
+ld.to_csv("final.csv",index=False)
