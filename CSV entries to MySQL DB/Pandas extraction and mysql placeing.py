60bfcf9fb1d84bbded4717e9ae6c348db65036e9
nikhil631 <nikhiltomar931@gmail.com>
2022-12-01 12:03:19 +0530

    changed something

--- CSV entries to MySQL DB/Pandas extraction and mysql placeing.py
+++ CSV entries to MySQL DB/Pandas extraction and mysql placeing.py
@@ -1,5 +1,5 @@
 import pandas as pd
 from sqlalchemy import create_engine
-engine = create_engine("mysql+mysqldb://root:"+'27682202'+"@localhost/nick")
+engine = create_engine("mysql+mysqldb://root:"+'1234567890'+"@localhost/nick")
 df=pd.read_csv("File.csv")
 df.to_sql(con=engine, name='information',if_exists='replace', index=False)
