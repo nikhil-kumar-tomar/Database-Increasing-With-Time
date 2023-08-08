839beb1db5a333da63b72e533e163c0128ea098f
nikhil631 <nikhiltomar931@gmail.com>
2023-08-08 10:48:21 +0530

    Personal student database management deleted

--- Personal App/personal/students/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class studs(models.Model):
-    name=models.CharField(max_length=400)
-    roll=models.IntegerField(primary_key=True,unique=True)
-    email=models.EmailField(max_length=300,null=True,unique=True)
-    def __str__(self):
-        return f"{self.roll},{self.name}"
